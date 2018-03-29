419f88b96256281675afbc7fc6961dc934658d19
MrHidalgo <Luckyhidalgo@gmail.com>
2018-03-29 15:40:15 +0300

    Slick slider - response breakpoint.

--- src/js/main.js
+++ src/js/main.js
@@ -196,6 +196,36 @@ $(document).ready(function(){
       infinite: true,
       slidesToShow: 8,
       slidesToScroll: 8,
+      responsive: [
+        {
+          breakpoint: 1440,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 6
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ],
       customPaging: function() {
         return '<div></div>';
       }
